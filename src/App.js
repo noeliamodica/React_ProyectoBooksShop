e3f5ab25af8faab258de6725e37ce4a465797ac8
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-19 16:05:48 -0300

    genero ruteo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from './components/Home/Home';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-       <Home />
+       <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
