094f9ead915924030781adab427b52d37daebc3e
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-19 18:38:49 -0300

    genero condicional para carrito vacio

--- src/components/CartContent/CartContent.js
+++ src/components/CartContent/CartContent.js
@@ -1,16 +1,25 @@
 import CartElements from "./CartElements";
 import CartTotal from "./CartTotal";
+import { useContext } from "react";
+import { dataContext } from "../Context/DataContext";
 
 
 const CartContent = () => {
+
+    const { cart } = useContext(dataContext);
     
   
-    return (
+    return cart.length > 0 ?    (
         <div>
             <CartElements />
             <CartTotal />
          </div>
+    ) : (
+        <h2 className="cart-message-center">
+            Your cart is empy
+        </h2>
     )
+    
   };
   
   export default CartContent;
